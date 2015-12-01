dacc2f85966dc7f2c19ef6e8265df8f1106a4ebe
jiwang <jiwang@alibaba-inc.com>
2015-12-01 23:55:14 +0800

    add user table creator sql

--- db/create_db.sql
+++ db/create_db.sql
@@ -1,2 +1,12 @@
 create database if not exists tieba;
 use tieba;
+
+create table if not exists user (
+    user_id int not null auto_increment,
+    user_name char(128) not null unique,
+    tieba_name char(128) default '',
+    admin_type enum('none', 'admin', 'little_admin', 'img_admin') default 'none',
+    following_num smallint default 0,
+    followed_num smallint default 0,
+    primary key (user_id)
+)default charset=utf8;
