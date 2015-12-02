b12da7963fc299e1f0a1563bb5e1719479efdba1
jiwang <jiwang@alibaba-inc.com>
2015-12-02 15:34:09 +0800

    create: following and followed tables among users, user-managing-tieba table

--- db/create_db.sql
+++ db/create_db.sql
@@ -2,11 +2,45 @@ create database if not exists tieba;
 use tieba;
 
 create table if not exists user (
-    user_id int not null auto_increment,
-    user_name char(128) not null unique,
-    tieba_name char(128) default '',
+    id int not null auto_increment,
+    name char(128) not null unique,
     admin_type enum('none', 'admin', 'little_admin', 'img_admin') default 'none',
     following_num smallint default 0,
     followed_num smallint default 0,
-    primary key (user_id)
+    primary key (id)
 )default charset=utf8;
+
+create table if not exists tieba (
+    id int not null auto_increment,
+    name char(128) not null unique,
+    followed_num int default 0,
+    admin_num smallint default 0,
+    slogan char(128) default '',
+    belong_dir char(128) default '',
+    primary key (id)
+)default charset=utf8;
+
+create table if not exists user_follow_tieba (
+    user_id int not null,
+    tieba_id int not null,
+    primary key (user_id, tieba_id)
+)default charset=utf8;
+
+create table if not exists user_manage_tieba (
+    user_id int not null,
+    tieba_id int not null,
+    primary key (user_id, tieba_id)
+)default charset=utf8;
+
+create table if not exists user_follow_user (
+    from_user_id int not null,
+    to_user_id int not null,
+    primary key (from_user_id, to_user_id)
+)default charset=utf8;
+
+create table if not exists user_followed_user (
+    from_user_id int not null,
+    to_user_id int not null,
+    primary key (from_user_id, to_user_id)
+)default charset=utf8;
+
