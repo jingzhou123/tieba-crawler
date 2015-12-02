f33a91126a7cfdc7559b7b81a680ec35b4f3af26
jiwang <jiwang@alibaba-inc.com>
2015-12-02 17:08:04 +0800

    db seeds

--- db/seed.sql
+++ db/seed.sql
@@ -1,6 +1,7 @@
-insert ignore user (name) values ('jz');
+insert ignore user (name) values('jz'), ('user_a');
 insert ignore tieba (name) values('北京邮电大学');
 insert ignore post values (default, 1, 1, default, 'title', 'body', '2015-12-3 11-11-11', default);
+insert ignore user_follow_tieba values(1, 1), (2, 1)
 
   
   
