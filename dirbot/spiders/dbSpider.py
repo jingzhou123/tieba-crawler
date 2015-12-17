0b5d9913084f95e2a2a73103a4ce592901a59724
jiwang <jiwang@alibaba-inc.com>
2015-12-17 18:00:35 +0800

    can fetch user home link

--- dirbot/spiders/dbSpider.py
+++ dirbot/spiders/dbSpider.py
@@ -1,6 +1,8 @@
 from dirbot.settings import MYSQL_HOST, MYSQL_USER, MYSQL_DBNAME, MYSQL_PASSWD
 from scrapy.spiders import Spider
+from scrapy import Request
 import MySQLdb
+import logging
 
 class DbSpider(Spider):
 
@@ -17,5 +19,36 @@ class DbSpider(Spider):
             use_unicode=True,
         )
         self.conn = MySQLdb.connect(**dbargs)
+    def get_query_sql(self):
+        """TODO: Docstring for get_query_sql.
+        :returns: TODO
+
+        """
+        pass
+
+    def query_records(self, start_index = 0, step = 50):
+        """TODO: Docstring for query_records.
+        :returns: yield a row from database, etc user's name
+
+        """
+
+
+    def url_from_row(self, row):
+        """TODO: Docstring for generate_url_from_row.
+        :returns: TODO
+
+        """
+        pass
+
+    def start_requests(self):
+        """TODO: Docstring for start_requests.
+        :returns: TODO
+
+        """
+
+        for row in self.query_records():
+            yield Request(self.url_from_row(row), callback=self.parse, meta={'row': row}) # do something with rows from database
+
+
 
 
--- dirbot/spiders/member.py
+++ dirbot/spiders/member.py
@@ -10,9 +10,33 @@ class MemberSpider(CookieSpider, DbSpider):
     request_url_tmpl = 'http://tieba.baidu.com/bawu2/platform/listMemberInfo?word=%s&ie=utf-8&pn=%s'
     name = 'member'
 
-    def start_requests(self):
-        """TODO: Docstring for start_requests.
+    def query_records(self, start_index = 0, num = 50):
+        """TODO: Docstring for query_records.
+
+        :start_index: TODO
+        :step: TODO
+        :returns: TODO
+
+        """
+        while True:
+            cursor = self.conn.cursor()
+            cursor.execute("""SELECT user_name from user_manage_tieba limit %s, %s""", (start_index, num))
+            rows = cursor.fetchall()
+
+            if rows:
+                for row in rows:
+                    yield row
+
+    def url_from_row(self, row):
+        return self.request_url_tmpl % (row[0], 1)
+
+    def parse(self, response):
+        """TODO: Docstring for parse.
+
+        :response: TODO
         :returns: TODO
 
         """
         pass
+
+
